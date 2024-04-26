f43fc8e7de42ac604421aa3bd9cb45dce7f8c1ce
geogrey12muiruri <greggambugua@gmail.com>
2024-04-26 16:25:01 -0700

    added userRoutes and controllers

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,15 @@ import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectMongoDB from '../db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
+import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 
 const app = express();
 
