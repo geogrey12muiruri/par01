4f0f9c35f95eff9fddc5d16f0c72de302dcefdae
geogrey12muiruri <greggambugua@gmail.com>
2024-04-26 15:07:02 -0700

    added the followunfollow functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
 import connectMongoDB from '../db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
