113952f0b4a704986309d42f84e46c9415f11bb5
geogrey12muiruri <greggambugua@gmail.com>
2024-04-26 17:06:59 -0700

    added the comment functionality

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
+import postRoutes from './routes/post.routes.js';
 import connectMongoDB from '../db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from "cloudinary";
@@ -26,6 +27,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);  
 
 app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
