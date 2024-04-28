b0d9f8cda2dd494502a7707f96f30c84d38ff302
geogrey12muiruri <greggambugua@gmail.com>
2024-04-28 01:07:48 -0700

    completed the notification routes and controller

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 import connectMongoDB from '../db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from "cloudinary";
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);  
+app.use("/api/notifications", notificationRoutes); 
 
 app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
