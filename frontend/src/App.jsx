ddf893a599a371f89ca3accf461090cc46f6bcfe
geogrey12muiruri <greggambugua@gmail.com>
2024-04-28 08:56:36 -0700

    completed the UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
-
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import LoginPage from './pages/auth/login/LoginPage'
+import SignUpPage from './pages/auth/signup/SignUpPage'
+import NotificationPage from './pages/notification/NotificationPage'
+import ProfilePage from './pages/profile/ProfilePage'
+import Sidebar from './components/Sidebar'
+import RightPanel from './components/common/RightPanel'
 
 function App() {
   
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+<div className='flex max-w-6xl mx-auto'>
+<Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+      <RightPanel />
+		</div>
+
   )
 }
 
