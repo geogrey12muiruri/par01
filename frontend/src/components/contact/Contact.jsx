17cd1d32e76f9762fb18541341fcb55bf2c053d2
Parrot <greggambugua@gmail.com>
2024-05-01 21:10:21 +0300

    Update Contact.jsx

---
+++ frontend/src/components/contact/Contact.jsx
@@ -0,0 +1,19 @@
+ import React from 'react'
+import {useSelector} from 'react-redux' 
+import HeroSection from '../components/hero/Hero'
+import ServicesSection from '../components/services/ServiceSection'
+import Blogs from '../components/blogs/Blogs'
+
+const HomePage = () => {
+  const {user} = useSelector((state)=> state.user);
+  return (
+    <div>
+    
+      <HeroSection/>
+      <ServicesSection/>
+      <Blogs/>
+    </div>
+  )
+}
+
+export default HomePage
